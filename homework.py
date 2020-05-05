61b863567a4b52eea27c6c097edbc8481873c07b
Sapraliev Mikhail <13bit57@gmail.com>
2020-05-05 17:23:32 +0300

    Release 0.1

--- homework.py
+++ homework.py
@@ -1,20 +1,30 @@
-import time
-
-import requests
+import requests, os, time
 from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_status(user_id):
     params = {
-        ...
+        'user_id': user_id,
+        'v': '5.92',
+        'fields': 'online',
+        'access_token': os.getenv('access_token')
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    status = requests.post('https://api.vk.com/method/users.get', params).json()['response'][0]['online']
+    return status
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    account_sid = os.getenv('account_sid')
+    auth_token = os.getenv('auth_token')
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to=os.getenv('NUMBER_TO'),
+        from_=os.getenv('NUMBER_FROM'),
+        body=sms_text
+    )
+    return message.sid
 
 
 if __name__ == "__main__":
